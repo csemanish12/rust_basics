a49983fce17049c4a9b8a1adcb91a6f244852367
Manish Sah <sahmanish20@gmail.com>
2024-10-16 19:03:49 +0545

    variable and mutability

---
+++ variables_and_mutability/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut a: i32 = 5; // by default its immutable
+
+    println!("The value of a is: {}", a);
+    a = 10;
+    println!("The value of a after modification is: {}", a);
+
+
+}
