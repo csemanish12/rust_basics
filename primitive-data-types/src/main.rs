9702b49238df70c574e434bf708999520199b4c1
Manish Sah <sahmanish20@gmail.com>
2024-10-16 12:11:06 +0545

    primitive data type

---
+++ primitive-data-types/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // Integers sections
+    let x: i32 = -50;
+    let y: u64 = 100;
+
+    println!("Signed Integer: {}", x);
+    println!("UnSigned Integer: {}", y);
+
+    // Floats
+    // f32, f64
+    let pi: f64 = 3.14;
+    println!("Value of pi: {}", pi);
+
+    // Boolean: true/false
+    let is_enabled: bool = true;
+    println!("is it enabled ? {}", is_enabled);
+
+    // Character type - char (unicode character)
+    let letter: char = 'a';
+    println!("First letter of alphabet: {}", letter);
+}
