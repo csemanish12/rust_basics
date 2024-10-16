f4ccbf40344fa92e14116c568bd480d041d67c83
Manish Sah <sahmanish20@gmail.com>
2024-10-16 19:11:44 +0545

    constants

---
+++ constants/src/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    let mut x = 5;
+    const Y: i32 = 10;
+    println!("Value of x is {} and y is {}", x, Y);
+    println!("Value of PI is {}", PI);
+}
+
+// you can declare constant in global namespace
+const PI: f64 = 3.141592;
