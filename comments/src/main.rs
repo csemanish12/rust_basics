f17836b3497320c09ac277a6105c54a171437f7b
Manish Sah <sahmanish20@gmail.com>
2024-10-17 07:51:43 +0545

    comments

---
+++ comments/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // this line will be ignored by compiler
+
+    // this line for print hello, world
+    println!("Hello, world!"); // this is a comment for printing hello world
+
+    // block commenting
+    /* 
+    this lets you write multiline comments
+    */
+}
