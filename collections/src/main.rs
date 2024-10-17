088deb20882c6745cb13d3a88886da14c7dad81e
Manish Sah <sahmanish20@gmail.com>
2024-10-17 19:17:40 +0545

    utf8

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -21,4 +21,25 @@ fn main() {
         None => println!("There is no second element"),
     }
 
+    // collection type - UTF8
+    let s = "whatever".to_string();
+
+    let s2 = String::from("whatever");
+    
+    let mut s3 = String::from("foo");
+    s3.push_str("bar"); // for string slice
+    s3.push('!'); // single quote for character
+    println!("The value of s3: {}", s3);
+
+    // concat
+    let s4 = s2 + &s3; // previous occurance of s2 is moved here as s4 took ownership of s2
+    // plus doesnot work with more than one owned variable
+
+    println!("s4 is: {}", s4);
+
+    // formatting string
+    let value1 = String::from("hello");
+    let value2 = String::from("world");
+    let full = format!("{value1} {value2}");
+    println!("full message is: {full}");
 }
